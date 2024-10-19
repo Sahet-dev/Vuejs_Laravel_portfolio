a01668df8cb212b804ff089ef38d0e3eafb1cb34
Sahetmyrat <kakajansahy@gmail.com>
2024-10-19 16:14:35 +0300

    Add github and LinkedIn

---
+++ resources/js/urlConfig.js
@@ -0,0 +1,18 @@
+
+export const BASE_URL = 'http://127.0.0.1:8000';
+
+export const IMAGE_PROFILE = `${BASE_URL}/storage/me.jpg`;
+
+
+export const CALCULATOR_IMAGE = `${BASE_URL}/storage/calculator.png`;
+export const DRUMMACHINE_IMAGE = `${BASE_URL}/storage/drummachine.png`;
+export const EngTm_IMAGE = `${BASE_URL}/storage/engtm.png`;
+export const MarkdownPreviewer_IMAGE = `${BASE_URL}/storage/markdown.png`;
+export const Pomodoro_IMAGE = `${BASE_URL}/storage/pomodoro.png`;
+export const RandomQuote_IMAGE = `${BASE_URL}/storage/randomquote.png`;
+export const SocialMediaApp_IMAGE = `${BASE_URL}/storage/socialapp.png`;
+
+
+
+
+
